4a794d94b5666b58ef0c2dc273f4ce3d3f6d7d4b
RobiDario <robertocorjuc@gmail.com>
2026-04-22 20:02:36 +0300

    Restored main application class

---
+++ src/main/java/lynx/team2/marketevent/MarketEventApplication.java
@@ -0,0 +1,11 @@
+package lynx.team2.marketevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketEventApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MarketEventApplication.class, args);
+    }
+}
